ed3b2bccc4cb4c4ea1d7306b781073bc5c080fda
duclong1311 <duclong1311.work@gmail.com>
2024-03-16 17:10:03 +0700

    solution

---
+++ RemoveVal_InArray.cs
@@ -0,0 +1,13 @@
+﻿//27. Remove Element
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++){
+            if (nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
