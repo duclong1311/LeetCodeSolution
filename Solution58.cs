4a437a1ef3f7f6a8052da11b47ac9b6d671bee83
duclong1311 <duclong1311.work@gmail.com>
2024-03-21 16:23:14 +0700

    Solution

---
+++ Solution58.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int LengthOfLastWord(string s) {
+        s = s.TrimEnd();
+        int count = 0;
+        for (int i = s.Length; i >= 0; i--){
+            if (s[i] != ' '){
+                count++; 
+            }
+            else 
+                break;
+        }
+        return count;
+    }
+}
